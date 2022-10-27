1a97c7bd39dec5c4d5f1164cfbaa6ae0d0a767d5
Gurleen <79988919+GurleenKaur22@users.noreply.github.com>
2022-10-27 11:57:11 +0530

    additions made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+
+import {ChannelListContainer, ChannelContainer} from './components';
+
 import './App.css';
 
-function App() {
+const apikey ='8yktqkshgt8z';
+
+const client = StreamChat.getInstance(apikey);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "app__wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer 
+            
+            />
+
+            <ChannelContainer 
+            
+            />
+        </Chat>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
